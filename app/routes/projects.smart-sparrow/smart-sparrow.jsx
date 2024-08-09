b6192fe7b9abe753c8213416a6c55cab9d4c0259
ATG218 <alextgarbuz@gmail.com>
2024-08-08 22:32:56 -0400

    Initial commit - Systems integration

---
+++ app/routes/projects.smart-sparrow/smart-sparrow.jsx
@@ -0,0 +1,339 @@
+import backgroundSprLarge from '~/assets/spr-background-large.jpg';
+import backgroundSprPlaceholder from '~/assets/spr-background-placeholder.jpg';
+import imageSprBackgroundVolcanismLarge from '~/assets/spr-background-volcanism-large.jpg';
+import imageSprBackgroundVolcanismPlaceholder from '~/assets/spr-background-volcanism-placeholder.jpg';
+import imageSprBackgroundVolcanism from '~/assets/spr-background-volcanism.jpg';
+import backgroundSpr from '~/assets/spr-background.jpg';
+import imageSprComponentsDarkLarge from '~/assets/spr-components-dark-large.png';
+import imageSprComponentsDarkPlaceholder from '~/assets/spr-components-dark-placeholder.png';
+import imageSprComponentsDark from '~/assets/spr-components-dark.png';
+import imageSprComponentsLightLarge from '~/assets/spr-components-light-large.png';
+import imageSprComponentsLightPlaceholder from '~/assets/spr-components-light-placeholder.png';
+import imageSprComponentsLight from '~/assets/spr-components-light.png';
+import imageSprDesignSystemDarkLarge from '~/assets/spr-design-system-dark-large.png';
+import imageSprDesignSystemDarkPlaceholder from '~/assets/spr-design-system-dark-placeholder.png';
+import imageSprDesignSystemDark from '~/assets/spr-design-system-dark.png';
+import imageSprDesignSystemLightLarge from '~/assets/spr-design-system-light-large.png';
+import imageSprDesignSystemLightPlaceholder from '~/assets/spr-design-system-light-placeholder.png';
+import imageSprDesignSystemLight from '~/assets/spr-design-system-light.png';
+import imageSprLessonBuilderDarkLarge from '~/assets/spr-lesson-builder-dark-large.jpg';
+import imageSprLessonBuilderDarkPlaceholder from '~/assets/spr-lesson-builder-dark-placeholder.jpg';
+import imageSprLessonBuilderDark from '~/assets/spr-lesson-builder-dark.jpg';
+import imageSprLessonBuilderLightLarge from '~/assets/spr-lesson-builder-light-large.jpg';
+import imageSprLessonBuilderLightPlaceholder from '~/assets/spr-lesson-builder-light-placeholder.jpg';
+import imageSprLessonBuilderLight from '~/assets/spr-lesson-builder-light.jpg';
+import videoSprMotionLarge from '~/assets/spr-motion-large.mp4';
+import videoSprMotionPlaceholder from '~/assets/spr-motion-placeholder.jpg';
+import videoSprMotion from '~/assets/spr-motion.mp4';
+import imageSprSchema1DarkLarge from '~/assets/spr-schema-1-dark-large.png';
+import imageSprSchema1DarkPlaceholder from '~/assets/spr-schema-1-dark-placeholder.png';
+import imageSprSchema1Dark from '~/assets/spr-schema-1-dark.png';
+import imageSprSchema1LightLarge from '~/assets/spr-schema-1-light-large.png';
+import imageSprSchema1LightPlaceholder from '~/assets/spr-schema-1-light-placeholder.png';
+import imageSprSchema1Light from '~/assets/spr-schema-1-light.png';
+import imageSprSchema2DarkLarge from '~/assets/spr-schema-2-dark-large.png';
+import imageSprSchema2DarkPlaceholder from '~/assets/spr-schema-2-dark-placeholder.png';
+import imageSprSchema2Dark from '~/assets/spr-schema-2-dark.png';
+import imageSprSchema2LightLarge from '~/assets/spr-schema-2-light-large.png';
+import imageSprSchema2LightPlaceholder from '~/assets/spr-schema-2-light-placeholder.png';
+import imageSprSchema2Light from '~/assets/spr-schema-2-light.png';
+import imageSprStoryboarderDarkLarge from '~/assets/spr-storyboarder-dark-large.png';
+import imageSprStoryboarderDarkPlaceholder from '~/assets/spr-storyboarder-dark-placeholder.png';
+import imageSprStoryboarderDark from '~/assets/spr-storyboarder-dark.png';
+import imageSprStoryboarderLightLarge from '~/assets/spr-storyboarder-light-large.png';
+import imageSprStoryboarderLightPlaceholder from '~/assets/spr-storyboarder-light-placeholder.png';
+import imageSprStoryboarderLight from '~/assets/spr-storyboarder-light.png';
+import { Footer } from '~/components/footer';
+import { Image } from '~/components/image';
+import { Link } from '~/components/link';
+import { SegmentedControl, SegmentedControlOption } from '~/components/segmented-control';
+import { ThemeProvider, useTheme } from '~/components/theme-provider';
+import {
+  ProjectBackground,
+  ProjectContainer,
+  ProjectHeader,
+  ProjectImage,
+  ProjectSection,
+  ProjectSectionColumns,
+  ProjectSectionContent,
+  ProjectSectionHeading,
+  ProjectSectionText,
+  ProjectTextRow,
+} from '~/layouts/project';
+import { baseMeta } from '~/utils/meta';
+import { Suspense, lazy, useMemo } from 'react';
+import { media } from '~/utils/style';
+import styles from './smart-sparrow.module.css';
+
+const title = 'Designing the future of education';
+const description =
+  'I worked as the design lead on a major iteration of Smart Sparrow’s product. We took the platform in a bold new direction, focusing on becoming the best tool for learning designers.';
+const roles = [
+  'Researcher',
+];
+
+export const meta = () => {
+  return baseMeta({ title, description, prefix: 'Projects' });
+};
+
+export const SmartSparrow = () => {
+  const { theme, toggleTheme } = useTheme();
+  const isDark = theme === 'dark';
+  const themes = ['dark', 'light'];
+
+  const handleThemeChange = index => {
+    toggleTheme(themes[index]);
+  };
+
+  return (
+    <>
+      <ProjectContainer>
+        <ProjectBackground
+          opacity={isDark ? 0.5 : 0.8}
+          src={backgroundSpr}
+          srcSet={`${backgroundSpr} 1080w, ${backgroundSprLarge} 2160w`}
+          placeholder={backgroundSprPlaceholder}
+        />
+        <ProjectHeader
+          title={title}
+          description={description}
+          url="https://www.smartsparrow.com/"
+          roles={roles}
+        />
+        <ProjectSection padding="top">
+          <ProjectSectionContent>
+            <ProjectImage
+              raised
+              key={theme}
+              srcSet={
+                isDark
+                  ? `${imageSprLessonBuilderDark} 1280w, ${imageSprLessonBuilderDarkLarge} 2560w`
+                  : `${imageSprLessonBuilderLight} 1280w, ${imageSprLessonBuilderLightLarge} 2560w`
+              }
+              width={1280}
+              height={800}
+              placeholder={
+                isDark
+                  ? imageSprLessonBuilderDarkPlaceholder
+                  : imageSprLessonBuilderLightPlaceholder
+              }
+              sizes={`(max-width: ${media.mobile}px) 100vw, (max-width: ${media.tablet}px) 800px, 1000px`}
+              alt="The aero lesson builder app dragging an audio component into a screen about plant cells."
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection>
+          <ProjectTextRow>
+            <ProjectSectionHeading>The problem</ProjectSectionHeading>
+            <ProjectSectionText>
+              In 2017, Smart Sparrow began a project to build an entirely new platform to
+              from the ground up to serve as the most powerful tool for educators to
+              create online learning experiences. The old platform was built in Flash, and
+              there were a number of user experience problems to solve in the process of
+              moving the platform to Javascript. The primary goals for the project were
+              reducing barriers to collaboration, and making the platform both easier for
+              new users, but with plenty of room to scale for advanced users.
+            </ProjectSectionText>
+          </ProjectTextRow>
+        </ProjectSection>
+        <ProjectSection light={isDark}>
+          <ProjectSectionContent>
+            <Image
+              key={theme}
+              srcSet={
+                isDark
+                  ? `${imageSprComponentsDark} 1024w, ${imageSprComponentsDarkLarge} 2048w`
+                  : `${imageSprComponentsLight} 1024w, ${imageSprComponentsLightLarge} 2048w`
+              }
+              width={1024}
+              hright={800}
+              placeholder={
+                isDark
+                  ? imageSprComponentsDarkPlaceholder
+                  : imageSprComponentsLightPlaceholder
+              }
+              alt={`A set of ${theme} themed components for the aero design system`}
+              sizes="100vw"
+            />
+            <ProjectTextRow>
+              <SegmentedControl
+                currentIndex={themes.indexOf(theme)}
+                onChange={handleThemeChange}
+              >
+                <SegmentedControlOption>Dark theme</SegmentedControlOption>
+                <SegmentedControlOption>Light theme</SegmentedControlOption>
+              </SegmentedControl>
+            </ProjectTextRow>
+            <ProjectTextRow>
+              <ProjectSectionHeading>The aero design system</ProjectSectionHeading>
+              <ProjectSectionText>
+                To streamline the design process across designers and engineers for such a
+                large project, it was important to lay the foundations with a strong,
+                flexible design system that could evolve during the product’s development
+                cycle. This would inform both the aesthetics and user experience across
+                the product itself as well as the website and marketing material.
+              </ProjectSectionText>
+            </ProjectTextRow>
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection>
+          <ProjectSectionContent>
+            <Image
+              raised
+              key={theme}
+              srcSet={
+                isDark
+                  ? `${imageSprDesignSystemDark} 1280w, ${imageSprDesignSystemDarkLarge} 2560w`
+                  : `${imageSprDesignSystemLight} 1280w, ${imageSprDesignSystemLightLarge} 2560w`
+              }
+              width={1280}
+              height={800}
+              placeholder={
+                isDark
+                  ? imageSprDesignSystemDarkPlaceholder
+                  : imageSprDesignSystemLightPlaceholder
+              }
+              alt="The homepage of the aero design system docs website linking to principles and components."
+              sizes="100vw"
+            />
+            <ProjectTextRow>
+              <ProjectSectionHeading>Design system docs</ProjectSectionHeading>
+              <ProjectSectionText>
+                A design system is useless if no one knows how to use it, so we put
+                together a comprehensive documentation website to cover principles, ux,
+                accessibility, and component guidelines for designers and engineers
+                working with the system.
+              </ProjectSectionText>
+            </ProjectTextRow>
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ThemeProvider theme="dark" data-invert>
+          <ProjectSection
+            backgroundOverlayOpacity={0.5}
+            backgroundElement={
+              <Image
+                srcSet={`${imageSprBackgroundVolcanism} 1280w, ${imageSprBackgroundVolcanismLarge} 2560w`}
+                width={1280}
+                height={900}
+                placeholder={imageSprBackgroundVolcanismPlaceholder}
+                alt="A dramatic ocean scene with lava forming a new land mass."
+                sizes="100vw"
+              />
+            }
+          >
+            <ProjectSectionColumns width="full">
+              <ProjectSectionContent width="full">
+                <ProjectTextRow width="s">
+                  <ProjectSectionHeading>Motion design</ProjectSectionHeading>
+                  <ProjectSectionText>
+                    Animation was a core principle in making the authoring experience a
+                    more understandable process. Elements animate in ways that indicate
+                    the cause and effect of each interaction to improve the fluidity of
+                    the overall experience.
+                  </ProjectSectionText>
+                </ProjectTextRow>
+              </ProjectSectionContent>
+              <Image
+                raised
+                className={styles.video}
+                srcSet={`${videoSprMotion} 1280w, ${videoSprMotionLarge} 2560w`}
+                width={1280}
+                height={800}
+                placeholder={videoSprMotionPlaceholder}
+                alt="A learning designer building and deploying an interactive lesson on volcanism using the app."
+                sizes={`(max-width: ${media.mobile}px) 100vw, 50vw`}
+              />
+            </ProjectSectionColumns>
+          </ProjectSection>
+        </ThemeProvider>
+        <ProjectSection>
+          <ProjectSectionContent>
+            <ProjectTextRow>
+              <ProjectSectionHeading>Encouraging adaptivity</ProjectSectionHeading>
+              <ProjectSectionText>
+                A major part of solving for collaboration was being able to visualize the
+                learner experience in the editor. This was especially beneficial for
+                subject matter experts and instructors need to review and give feedback on
+                the higher level structure without having to dig through all of the
+                adaptivity scenarios screen by screen.
+              </ProjectSectionText>
+            </ProjectTextRow>
+            <Image
+              raised
+              key={theme}
+              srcSet={
+                isDark
+                  ? `${imageSprStoryboarderDark} 1280w, ${imageSprStoryboarderDarkLarge} 2560w`
+                  : `${imageSprStoryboarderLight} 1280w, ${imageSprStoryboarderLightLarge} 2560w`
+              }
+              width={1280}
+              height={800}
+              placeholder={
+                isDark
+                  ? imageSprStoryboarderDarkPlaceholder
+                  : imageSprStoryboarderLightPlaceholder
+              }
+              alt="A drag and drop storyboard style editor for creating an adaptive lesson."
+              sizes={`(max-width: ${media.mobile}px) 100vw, 80vw`}
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection>
+          <ProjectSectionColumns>
+            <ProjectSectionContent>
+              <ProjectTextRow>
+                <ProjectSectionHeading>
+                  An extensible plugin ecosystem usable by everyone
+                </ProjectSectionHeading>
+                <ProjectSectionText>
+                  The most powerful aspect of the platform is the ability to create custom
+                  plugins for any content, whether it be a degree, course, lesson, screen,
+                  or interactive component. Out of the box these can be made configurable
+                  with minimal effort from developers. Learning designers can then edit
+                  everything using a common configuration interface.
+                </ProjectSectionText>
+              </ProjectTextRow>
+            </ProjectSectionContent>
+            <div className={styles.sidebarImages}>
+              <Image
+                className={styles.sidebarImage}
+                srcSet={
+                  isDark
+                    ? `${imageSprSchema2Dark} 260w, ${imageSprSchema2DarkLarge} 520w`
+                    : `${imageSprSchema2Light} 260w, ${imageSprSchema2LightLarge} 520w`
+                }
+                width={260}
+                height={660}
+                placeholder={
+                  isDark
+                    ? imageSprSchema2DarkPlaceholder
+                    : imageSprSchema2LightPlaceholder
+                }
+                alt="Configuration options for a component."
+                sizes={`(max-width: ${media.mobile}px) 50vw, 25vw`}
+              />
+              <Image
+                className={styles.sidebarImage}
+                srcSet={
+                  isDark
+                    ? `${imageSprSchema1Dark} 260w, ${imageSprSchema1DarkLarge} 520w`
+                    : `${imageSprSchema1Light} 260w, ${imageSprSchema1LightLarge} 520w`
+                }
+                width={260}
+                height={660}
+                placeholder={
+                  isDark
+                    ? imageSprSchema1DarkPlaceholder
+                    : imageSprSchema1LightPlaceholder
+                }
+                alt="Configuration options for text."
+                sizes={`(max-width: ${media.mobile}px) 50vw, 25vw`}
+              />
+            </div>
+          </ProjectSectionColumns>
+        </ProjectSection>
+      </ProjectContainer>
+      <Footer />
+    </>
+  );
+};
